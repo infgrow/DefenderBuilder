9012c42286a1db5018c814ea5f181cd24c15e5b7
ML <allen840922@gmail.com>
2022-11-24 18:17:51 +0900

    Update Building Type Scriptable Objects

--- DefenderBuilder/Assets/Scripts/BuildingManager.cs
+++ DefenderBuilder/Assets/Scripts/BuildingManager.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    [SerializeField] private Transform pfWoodHarvester;
-    Camera mainCamera;
+    
+    private Camera mainCamera;
+    private BuildingTypeListSO buildingTypeList;
+    private BuildingTypeSO buildingType;
     
     private void Start() {
         mainCamera = Camera.main;
+
+        buildingTypeList = Resources.Load<BuildingTypeListSO>("BuildingTypeList");
+        buildingType = buildingTypeList.list[0];
     }
     private void Update() {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            buildingType = buildingTypeList.list[0];
+        } 
+        else if (Input.GetKeyDown(KeyCode.Y)) {
+            buildingType = buildingTypeList.list[1];
         }
     }
 
