88c7258f375bf5238d8e77395d20fc3b3e9ebd1c
ML <allen840922@gmail.com>
2022-11-25 21:20:57 +0900

    Gold mine updated

--- DefenderBuilder/Assets/Scripts/BuildingManager.cs
+++ DefenderBuilder/Assets/Scripts/BuildingManager.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingManager : MonoBehaviour
-{
-    [SerializeField] private Transform pfWoodHarvester;
-    Camera mainCamera;
-    
+public class BuildingManager : MonoBehaviour {
+
+    private Camera mainCamera;
+    private BuildingTypeListSO buildingTypeList;
+    private BuildingTypeSO buildingType;
+
     private void Start() {
         mainCamera = Camera.main;
+
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        buildingType = buildingTypeList.list[0];
     }
     private void Update() {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            buildingType = buildingTypeList.list[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            buildingType = buildingTypeList.list[1];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            buildingType = buildingTypeList.list[2];
         }
     }
 
     private Vector3 GetMouseWorldPosition() {
-        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPosition.z = 0f;
-        return mouseWorldPosition;
+        Vector3 mousePoint = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePoint.z = 0f;
+        return mousePoint;
     }
 }
